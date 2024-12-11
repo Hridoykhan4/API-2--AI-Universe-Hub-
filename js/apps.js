a596cb3e732127d626c5e48d47a56565ec5604a3
Hridoykhan4 <hridoykhan148385@gmail.com>
2024-12-11 18:49:20 +0600

    practice project cleared

--- js/apps.js
+++ js/apps.js
@@ -50,24 +50,25 @@ const showAllCopilot = (dataDetails, showAll) => {
         `;
     cardContainer.appendChild(cardDiv);
   });
+toggleLoadingSpinner(false)
+
 };
 
-
-const showDetails = async(id) => {
-
-
-    const res = await fetch(`https://openapi.programming-hero.com/api/ai/tool/${id}`);
-    const data = await res.json();
-    const showByOne = data.data;
-    showSingleDetails(showByOne)
-}
+const showDetails = async (id) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/ai/tool/${id}`
+  );
+  const data = await res.json();
+  const showByOne = data.data;
+  showSingleDetails(showByOne);
+};
 
 const showSingleDetails = (card) => {
-    console.log(card)
+  console.log(card);
 
-    const showPopup = document.getElementById('show-details-popup');
+  const showPopup = document.getElementById("show-details-popup");
 
-    showPopup.innerHTML = `
+  showPopup.innerHTML = `
         <div class="flex-1 border border-red-500 rounded-lg bg-red-300 p-5 bg-opacity-5">
             <p class="font-bold">${card.description}</p>
             <div class="mt-4 grid grid-cols-2 gap-3">
@@ -78,13 +79,13 @@ const showSingleDetails = (card) => {
             <span>
             </div>
 
-            <div class="flex flex-col font-bold text-green-500">
+            <div class="flex flex-col font-bold text-orange-500">
             <p>${card.pricing[1]?.price}</p>
             <p>${card.pricing[1]?.plan}</p>
             <span>
             </div>
 
-            <div class="flex flex-col font-bold text-green-500">
+            <div class="flex flex-col font-bold text-red-500">
             <p>${card.pricing[2]?.price}</p>
             <p>${card.pricing[2]?.plan}</p>
             <span>
@@ -92,12 +93,20 @@ const showSingleDetails = (card) => {
           
             </div>
 
-            <div class="flex gap-2">
-                <div>
-                    <h3>${card.features.feature_name}</h3>
+            <div class="grid grid-cols-2 gap-6 mt-3">
+                <div class="flex-col">
+                <h3 class="font-bold text-xl">Features</h3>
+                    <li>${card.features[1].feature_name}</li>
+                    <li>${card.features[2].feature_name}</li>
+                    <li>${card.features[3].feature_name}</li>
                 </div>
                 <div>
-                
+                <h3 class="font-bold text-xl">Integration</h3>
+                    <li>${card?.integrations[0]}</li>
+                    <li>${card?.integrations[1]}</li>
+                    <li>${card?.integrations[2]}</li>
+                    <li>${card?.integrations[3]}</li>
+                  
                 </div>
             </div>
 
@@ -105,17 +114,28 @@ const showSingleDetails = (card) => {
         </div>
 
         <div class="flex-1">
-         <img src="${card.image_link[0] || card.image_link[1] || card.image_link[2]}" class="h-1/2 w-full" alt="Not Found">
+         <img src="${
+           card.image_link[0] || card.image_link[1] || card.image_link[2]
+         }" class="h-1/2 w-full" alt="Not Found">
             <div class="flex flex-col text-center mt-3 gap-3">
          <h3 class="font-bold">Hi, how are you doing today?</h3>
          <p class="font-semibold text-gray-700">I'm doing well, thank you for asking. How can <br> I assist you today?</p>
         </div>
         </div>
-    `
-    my_modal_5.showModal()
-}
+    `;
+  my_modal_5.showModal();
+};
 
 
+const toggleLoadingSpinner = (isLoading) => {
+  const spinLoader = document.getElementById('spinnerControl');
+  if (isLoading) {
+    spinLoader.classList.remove("hidden");
+  } else {
+    spinLoader.classList.add("hidden");
+  }
+};
+toggleLoadingSpinner(true)
 
 loadAI();
 
